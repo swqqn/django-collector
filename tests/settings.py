67f0325d54e0157b082b4b774fd30b41248f9743
swqqn <mail2hack@yandex.ru>
2011-07-24 19:16:57 +0000

    initial attempt at unit tests using nose

--- tests/settings.py
+++ tests/settings.py
@@ -1,19 +1,6 @@
-# -*- coding: utf-8 -*-
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'tests.db',
+        'NAME': 'collector.db',
     }
 }
-
-INSTALLED_APPS = ('collector',)
-
-ROOT_URLCONF = 'tests.urls'
-
-
-# Local Variables:
-# indent-tabs-mode: nil
-# End:
-# vim: ai et sw=4 ts=4
