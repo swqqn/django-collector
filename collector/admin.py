d492a424b2726f37330fc6c07f7e947367713252
swqqn <mail2hack@yandex.ru>
2011-07-24 17:43:35 +0000

    initial blob module to capture e-mail addresses

--- collector/admin.py
+++ collector/admin.py
@@ -1,14 +1,4 @@
-# -*- coding: utf-8 -*-
-
-
 from django.contrib import admin
-from collector.models import Blob
-
+from collector.blobs.models import Blob
 
 admin.site.register(Blob)
-
-
-# Local Variables:
-# indent-tabs-mode: nil
-# End:
-# vim: ai et sw=4 ts=4
