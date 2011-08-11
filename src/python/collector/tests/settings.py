9663e8cf93b3d34e113ffc73c2614696790f92bd
gshn <mail2hack@yandex.ru>
2011-08-11 16:55:33 +0000

    so that tests will run in new directory layout

---
+++ src/python/collector/tests/settings.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+
+import os
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.environ['COLLECTOR_DB'] + os.sep + 'tests.db',
+    }
+}
+
+INSTALLED_APPS = ('collector',)
+
+ROOT_URLCONF = 'tests.urls'
+
+
+# Local Variables:
+# indent-tabs-mode: nil
+# End:
+# vim: ai et sw=4 ts=4
