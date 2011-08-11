f256916ffde8847f1717a25dd5b973f77ec55689
swqqn <mail2hack@yandex.ru>
2011-08-11 22:00:08 +0000

    provide default wrapper scripts around django's manage.py

--- src/python/collector/settings.py
+++ src/python/collector/settings.py
@@ -1,37 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import os
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.environ['COLLECTOR_DB'] + os.sep + 'collector.db',
-        }
-    }
-
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    )
-
-TEMPLATE_LOADERS = (
-    'django.template.loaders.app_directories.Loader',
-    )
-
-LANGUAGE_CODE = 'en'
-
-_ = lambda x: x
-
-LANGUAGES = (
-    ('en', _(u'English')),
-    ('es', _(u'Espanol')),
-    )
-
-APPEND_SLASH = False
-
-INSTALLED_APPS = ('collector',)
-
 # Local Variables:
 # indent-tabs-mode: nil
 # End:
