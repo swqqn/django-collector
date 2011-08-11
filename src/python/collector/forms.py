071761eda42bb9bd1ff42d5b6a209167b00e75d8
gshn <mail2hack@yandex.ru>
2011-08-11 16:10:29 +0000

    initial attempt at improved directory layout

--- src/python/collector/forms.py
+++ src/python/collector/forms.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
+
 from django.forms import Form, EmailField
 
 
 class CollectorForm(Form):
     collectorEmail = EmailField()
+
 
 # Local Variables:
 # indent-tabs-mode: nil
