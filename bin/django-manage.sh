1ecef096f9859f6d21bf39cbd9c9c63b53d154e2
gshn <mail2hack@yandex.ru>
2011-08-11 22:00:08 +0000

    provide default wrapper scripts around django's manage.py

--- bin/django-manage.sh
+++ bin/django-manage.sh
@@ -5,7 +5,7 @@ export COLLECTOR_HOME
 
 . "${COLLECTOR_HOME}"/etc/common
 
-"${COLLECTOR_BIN}"/python.sh "${COLLECTOR_BIN}"/django-manage.py "$@" -v 0
+"${COLLECTOR_BIN}"/python.sh "${COLLECTOR_BIN}"/django-manage.py "$@" --settings="${DJANGO_SETTINGS_MODULE}" -v 0
 
 # Local Variables:
 # indent-tabs-mode: nil
